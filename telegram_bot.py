22e680b51adf765997f9765995fe80e040cc5a40
Sasha <alexandersmirnovjr@gmail.com>
2022-11-11 18:24:19 +0500

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,4 +4,5 @@ import telegram
 bot = telegram.Bot(token='5762502045:AAF_0v5JWQUSw1CzTmdGDGN3KW0zkj2z8aY')
 print(bot.get_me())
 {"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"}
-bot.send_message(text='Hi everyone!', chat_id='@bebra228222')
+bot.send_message(text='Hi everyone!', chat_id='@space_download')
+bot.send_document(chat_id='@space_download', document=open('images/22nasa_apod.gif', 'rb'))
