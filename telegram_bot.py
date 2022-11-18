2708eea683cb5d02073548fe84734206d03ba863
Sasha <alexandersmirnovjr@gmail.com>
2022-11-18 18:18:59 +0500

    automatic publish photos to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,17 @@
 import telegram
+import os
+from time import sleep
 
-def auto_post_photos_to_telegram(chat_id, path):
+def post_photos_to_telegram(chat_id, path):
 
-    bot.send_photo(chat_id='@space_download', photo=open('images/2nasa_apod.gif', 'rb'))
+    bot.send_photo(chat_id='@space_download', photo=open(path, 'rb'))
 
 
 if __name__ == "__main__":
-
     bot = telegram.Bot(token='5762502045:AAF_0v5JWQUSw1CzTmdGDGN3KW0zkj2z8aY')
-    print(bot.get_me())
-    {"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"}
-    bot.send_message(text='Hi everyone!', chat_id='@space_download')
-    auto_post_photos_to_telegram('@space_download', 'images/2nasa_apod.gif')
+    files_in_dirs = os.listdir('images')
+    for files_in_dir in files_in_dirs:
+        path = os.path.join(files_in_dir)
+        file = os.path.join(str('images'), path)
+        post_photos_to_telegram('@space_download', file)
+        sleep(14400)
